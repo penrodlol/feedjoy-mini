c86746ffedfdada10ac629d8add9f7a2d4a0c31e
penrodlol <christian@penrod.contact>
2023-01-22 18:58:35 -0500

    ✨ Batch fetch read/write feeds | create latest 20 posts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,26 @@
 import chalk from 'chalk';
 import { writeFileSync } from 'fs';
-import type { Output } from 'rss-parser';
-import Parser from 'rss-parser';
-import urls from '../public/input.json';
+import { emptyDirSync } from 'fs-extra';
+import { flat, sift, sort } from 'radash';
+import upsertFeeds from './upsert-feeds';
 
-const parser = new Parser();
-const feeds: Array<Output<unknown>> = [];
+emptyDirSync('public/feeds');
 
-console.log(chalk.blue('\nPARSING URLS...\n'));
+console.log(chalk.blue('\nFETCHING URLS...\n'));
 
-await Promise.all(
-  urls.map(async (url) =>
-    parser
-      .parseURL(url)
-      .then((feed) => {
-        console.log(chalk.blue(`✅ ${url}`));
-        feeds.push(feed);
-      })
-      .catch((error) => console.error(chalk.red(`ERROR: ${url}/n${error}`))),
-  ),
-);
+const feeds = sift(await upsertFeeds());
 
-const payload = feeds
-  .flatMap((feed) =>
-    feed.items
-      .filter((post) => feed.title && post.title && post.link && post.pubDate)
-      .map((post) => ({
-        site: feed.title,
-        title: post.title,
-        link: post.link,
-        pubDate: new Date(post.pubDate as string),
-      })),
-  )
-  .sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+console.log(chalk.blue('\nFETCHING LATEST...\n'));
 
 try {
-  writeFileSync('./public/output.json', JSON.stringify(payload, null, 2));
-  console.log(chalk.blue('\nCOMPLETE!\n'));
-} catch (error) {
-  console.error(chalk.red(error));
+  const flattened = flat(feeds.map((feed) => feed.items));
+  const sorted = sort(flattened, (feed) => feed.pubDate.valueOf(), true);
+  const latest = sorted.slice(0, 20);
+
+  writeFileSync('public/feeds/latest.json', JSON.stringify(latest, null, 2));
+  console.log('✅ Latest 20 posts');
+} catch (_e) {
+  console.error('❌ Latest 20 posts');
 }
+
+console.log(chalk.blue('\nCOMPLETE!\n'));
---
+++ src/upsert-feeds.ts
@@ -0,0 +1,43 @@
+import { writeFileSync } from 'fs';
+import { parallel, unique } from 'radash';
+import Parser from 'rss-parser';
+import slugify from 'slugify';
+import urls from '../public/input.json';
+
+const parser = new Parser();
+const batchSize = 5;
+
+export default async () => {
+  return parallel(
+    batchSize,
+    unique(urls.map((url) => url.trim().replace(/www\./, ''))),
+    async (url) =>
+      parser
+        .parseURL(url)
+        .then(({ title, link, items: _items }) => {
+          const slug = slugify(url, { lower: true, strict: true });
+          const path = `public/feeds/${slug}.json`;
+          const items = _items
+            .filter((item) => item.title && item.link && item.pubDate)
+            .map((item) => ({
+              title: item.title as string,
+              link: item.link as string,
+              pubDate: new Date(item.pubDate as string),
+            }));
+          const payload = { title, link, items };
+
+          try {
+            writeFileSync(path, JSON.stringify(payload, null, 2));
+            console.log(`✅ ${url}`);
+            return payload;
+          } catch (_e) {
+            console.error(`❌ ${url}`);
+            return null;
+          }
+        })
+        .catch(() => {
+          console.error(`❌ ${url}`);
+          return null;
+        }),
+  );
+};
