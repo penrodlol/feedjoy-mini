f79a6cf318c0deea20938c2bb55cf8d43ddff5e7
penrodlol <christian@penrod.contact>
2023-01-25 20:26:13 -0500

    🐛 Workflow need assertion

--- src/utils/get-feeds.ts
+++ src/utils/get-feeds.ts
@@ -1,14 +1,14 @@
-import dayjs from "dayjs";
-import { parallel, sift, unique } from "radash";
-import Parser from "rss-parser";
-import slugify from "slugify";
-import _urls from "../../assets/urls.json";
+import dayjs from 'dayjs';
+import { parallel, sift, unique } from 'radash';
+import Parser from 'rss-parser';
+import slugify from 'slugify';
+import _urls from '../../assets/urls.json' assert { type: 'json' };
 
 export type GetFeeds = Awaited<ReturnType<typeof getFeeds>>;
 
 export const getFeeds = async () => {
   const parser = new Parser();
-  const urls = unique(_urls.map((url) => url.trim().replace(/www\./, "")));
+  const urls = unique(_urls.map((url) => url.trim().replace(/www\./, '')));
   const payload = await parallel(5, urls, async (url) =>
     parser
       .parseURL(url)
@@ -29,7 +29,7 @@ export const getFeeds = async () => {
             .filter((item) => item.title && item.link && item.pubDate)
             .map(({ title, link, pubDate: _pubDate }) => {
               const pubDate = new Date(_pubDate as string);
-              const formattedPubDate = dayjs(pubDate).format("MMM D, YYYY");
+              const formattedPubDate = dayjs(pubDate).format('MMM D, YYYY');
 
               return { site, title, link, pubDate, formattedPubDate };
             })
@@ -39,7 +39,7 @@ export const getFeeds = async () => {
       .catch(() => {
         console.error(`❌ ${url}`);
         return null;
-      })
+      }),
   );
 
   return sift(payload);
