616d4d175c2ec2efc306b7f3fde97c5656a745eb
penrodlol <christian@penrod.contact>
2023-01-26 21:35:02 -0500

    🚧 I want a more condensed layout so skipping images for now

--- src/utils/set-readme.ts
+++ src/utils/set-readme.ts
@@ -1,21 +1,14 @@
-import { load } from 'cheerio';
 import { renderFile } from 'ejs';
 import { writeFileSync } from 'fs';
-import fetch from 'node-fetch';
 import { join } from 'path';
-import { flat, parallel, sort } from 'radash';
+import { flat, sort } from 'radash';
 import type { GetFeeds } from './get-feeds';
 
 export const setReadme = async (feeds: GetFeeds) => {
   try {
     const flattened = flat(feeds.map((feed) => feed.items));
     const sorted = sort(flattened, (feed) => feed.pubDate.valueOf(), true);
-    const posts = await parallel(5, sorted.slice(0, 30), async (post) => {
-      const $ = load(await fetch(post.link as string).then((r) => r.text()));
-      const image = $('meta[property="og:image"]').attr('content');
-
-      return { ...post, image };
-    });
+    const posts = sorted.slice(0, 30);
 
     const path = join(process.cwd(), 'src/templates/README.ejs');
     writeFileSync('README.md', await renderFile(path, { posts }));
